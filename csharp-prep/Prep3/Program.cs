797438dffc6c328a69339effb3ac54283d4c639b
jamfrancis <145678104+jamfrancis@users.noreply.github.com>
2024-09-21 09:30:50 -0600

    Completed step 1 of prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magic = Console.ReadLine();
+        int number = int.Parse(magic);
+
+        Console.Write("What is your guess? ");
+        string response = Console.ReadLine();
+        int user = int.Parse(response);
+
+        // while (user != number)
+        // {
+            if (user < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            if (user > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        // }
     }
 }
