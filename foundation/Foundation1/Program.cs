f5200cad408baaa2142a0248171db065b27e76dc
jamfrancis <145678104+jamfrancis@users.noreply.github.com>
2024-10-12 19:56:43 -0600

    Completed Foundation 1

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("I can't believe THIS happened!", "Tony Baker", 400);
+        video1.AddComment("Alice", "This is amazing!");
+        video1.AddComment("Bob", "Wow, I can't believe it!");
+        video1.AddComment("Sam", "What happened?");
+
+        Video video2 = new Video("I spent $5 on THIS Tesla!", "Mike Brown", 350);
+        video2.AddComment("Charlie", "Incredible deal!");
+        video2.AddComment("Greg", "I love telsa");
+        video2.AddComment("Horace", "First");
+
+        Video video3 = new Video("I ate 12 Pizzas!", "Joey Smith", 1800);
+        video3.AddComment("David", "That's insane!");
+        video3.AddComment("Pam", "This is making me hungry!");
+        video3.AddComment("Bruce", "I love Pizza Hut");
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"{video.GetDisplayText()}\n");
+        }
     }
+
 }
