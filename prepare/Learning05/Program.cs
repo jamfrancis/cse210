2509f61ff03b958bca036b3fa4348743c430fb2b
jamfrancis <145678104+jamfrancis@users.noreply.github.com>
2024-10-15 18:26:30 -0600

    Completed the Learning Activity 5 about inheritance

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        MathAssignment a1 = new MathAssignment("John", "Integrals", "7.1", "41, 42, 43-50");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine(a1.GetHomeworkList());
+
+        WritingAssignment a2 = new WritingAssignment("Samantha Thompson", "Midgets", "A Short History about Midgets");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetWritingInformation());
     }
 }
